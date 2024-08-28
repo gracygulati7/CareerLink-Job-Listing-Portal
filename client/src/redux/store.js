1471d1c4c1db2c550d1a9a05c2045e9c10c64fa0
Gracy Gulati <gracy331.be22@chitkara.edu.in>
2024-08-28 10:52:17 +0530

    added

---
+++ client/src/redux/store.js
@@ -0,0 +1,19 @@
+import { combineReducers } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { jobsReducer } from "./reducers/jobReducer";
+import { loaderReducer } from "./reducers/loaderReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { usersReducer } from "./reducers/userReducer";
+
+
+const rootReducer = combineReducers({
+
+    jobsReducer: jobsReducer,
+    loaderReducer: loaderReducer,
+    usersReducer: usersReducer,
+
+})
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
